be2b37d04ea81741b984daf16445c6afc9b154c6
Whit Morriss <d.w.morriss@gmail.com>
2007-11-08 22:51:55 +0000

    initial commit
    git-svn-id: https://svn.socialplanning.org/svn/opencore.js/trunk@10730 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(name='opencore.js',
+      version=version,
+      description="javascript and zcml registrations required for opencore",
+      long_description="""\
+""",
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "Framework :: Zope3",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='javascript zcml',
+      author='opencore dev team',
+      author_email='opencore-dev@lists.openplans.org',
+      url='http://www.openplans.org/projects/opencore',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['opencore'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
